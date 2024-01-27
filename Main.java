7e939a55b27eab2e396bc407d4c6ff593a85a129
Tyler Knoop <143290179+Monotude@users.noreply.github.com>
2024-01-26 22:18:31 -0500

    final

--- Main.java
+++ Main.java
@@ -1,68 +1,59 @@
 import java.io.FileWriter;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.PriorityQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Main
 {
     private static final int THREAD_COUNT = 8;
     private static final int MAX_PRIME_NUMBER = 100000000;
-    private static final int NUMBER_OF_PRIMES_TO_WRITE = 10;
 
     public static void main(String[] args)
     {
-        long startTimeNanoseconds = System.nanoTime();
-
-        PrimeFinder primeFinder = new PrimeFinder(THREAD_COUNT, MAX_PRIME_NUMBER);
-        primeFinder.findPrimes();
-
-        long endTimeNanoseconds = System.nanoTime();
-
-        long runTimeMilliseconds = (endTimeNanoseconds - startTimeNanoseconds) / 1000000;
-
-        primeFinder.writeToFile(runTimeMilliseconds, NUMBER_OF_PRIMES_TO_WRITE);
+        PrimeFinder primeFinder = new PrimeFinder(MAX_PRIME_NUMBER);
+        primeFinder.findPrimes(THREAD_COUNT);
     }
 }
 
 class PrimeFinder
 {
-    private final Thread[] threads;
-    private final AtomicBoolean[] isPrime;
-    private final int maxPrimeFactor;
+    private static final int NUMBER_OF_PRIMES_TO_WRITE = 10;
+    private final PriorityQueue<Integer> primeNumbers;
+    private final AtomicInteger primeCount;
+    private final AtomicLong primeSum;
+    private final int maxPrimeNumber;
     private int counter;
-    private int primeCount;
-    private long primeSum;
 
-    public PrimeFinder(int threadCount, int maxPrimeNumber)
+    public PrimeFinder(int maxPrimeNumber)
     {
-        threads = new Thread[threadCount];
-        isPrime = new AtomicBoolean[maxPrimeNumber + 1];
-        for (int i = 0; i < isPrime.length; ++i)
+        this.primeNumbers = new PriorityQueue<>();
+        this.maxPrimeNumber = maxPrimeNumber;
+        this.counter = 2;
+        this.primeCount = new AtomicInteger();
+        this.primeSum = new AtomicLong();
+    }
+
+    public void addPrimeNumber(int number)
+    {
+        synchronized (primeNumbers)
         {
-            isPrime[i] = new AtomicBoolean(true);
+            primeNumbers.add(number);
+
+            if (primeNumbers.size() > NUMBER_OF_PRIMES_TO_WRITE)
+            {
+                primeNumbers.poll();
+            }
         }
-        maxPrimeFactor = (int) Math.sqrt(maxPrimeNumber);
-        counter = 2;
-        primeCount = 0;
-        primeSum = 0;
     }
 
     public int getMaxPrimeNumber()
     {
-        return isPrime.length - 1;
-    }
-
-    public int getMaxPrimeFactor()
-    {
-        return maxPrimeFactor;
+        return maxPrimeNumber;
     }
 
     public synchronized int getAndIncrementCounter()
     {
-        while (counter <= maxPrimeFactor && !isPrime[counter].get())
-        {
-            ++counter;
-        }
-
-        if (counter <= maxPrimeFactor)
+        if (counter <= maxPrimeNumber)
         {
             return counter++;
         }
@@ -70,31 +61,34 @@ class PrimeFinder
         return counter;
     }
 
-    public void setIsPrime(int number, boolean expectedValue, boolean changedValue)
+    public void incrementPrimeCount()
     {
-        isPrime[number].compareAndSet(expectedValue, changedValue);
+        primeCount.getAndIncrement();
     }
 
-    public void findPrimes()
+    public void incrementPrimeSum(int number)
     {
-        isPrime[0].set(false);
-        isPrime[1].set(false);
-
-        runThreads();
-
-        for (int i = 0; i < isPrime.length; ++i)
-        {
-            if (isPrime[i].get())
-            {
-                ++primeCount;
-                primeSum += i;
-            }
-        }
+        primeSum.getAndAdd(number);
     }
 
-    private void runThreads()
+    public void findPrimes(int threadCount)
     {
-        for (int i = 0; i < threads.length; ++i)
+        long startTimeNanoseconds = System.nanoTime();
+
+        runThreads(threadCount);
+
+        long endTimeNanoseconds = System.nanoTime();
+
+        long runTimeMilliseconds = (endTimeNanoseconds - startTimeNanoseconds) / 1000000;
+
+        writeToFile(runTimeMilliseconds);
+    }
+
+    private void runThreads(int threadCount)
+    {
+        Thread[] threads = new Thread[threadCount];
+
+        for (int i = 0; i < threadCount; ++i)
         {
             threads[i] = new Thread(new PrimeFinderThread(this));
             threads[i].start();
@@ -112,7 +106,7 @@ class PrimeFinder
         }
     }
 
-    public void writeToFile(long runtime, int numberOfPrimesToWrite)
+    private void writeToFile(long runtime)
     {
         try (FileWriter primesFileWriter = new FileWriter("primes.txt"))
         {
@@ -122,33 +116,20 @@ class PrimeFinder
 
             primesFileWriter.write("Prime sum: " + primeSum + "\n");
 
-            writeLargestPrimes(primesFileWriter, numberOfPrimesToWrite);
+            writeLargestPrimes(primesFileWriter);
         } catch (Exception exception)
         {
             System.out.println("File error has occurred");
         }
     }
 
-    private void writeLargestPrimes(FileWriter primesFileWriter, int numberOfPrimesToWrite)
+    private void writeLargestPrimes(FileWriter primesFileWriter)
     {
-        int[] primesToWrite = new int[numberOfPrimesToWrite];
-        for (int i = getMaxPrimeNumber(); i >= 0; --i)
-        {
-            if (numberOfPrimesToWrite == 0)
-            {
-                break;
-            }
-
-            if (isPrime[i].get())
-            {
-                primesToWrite[--numberOfPrimesToWrite] = i;
-            }
-        }
-        for (int prime : primesToWrite)
+        for (int i = NUMBER_OF_PRIMES_TO_WRITE; !primeNumbers.isEmpty() && i > 0; --i)
         {
             try
             {
-                primesFileWriter.write(prime + "\n");
+                primesFileWriter.write(primeNumbers.remove() + "\n");
             } catch (Exception exception)
             {
                 System.out.println("File error has occurred");
@@ -169,19 +150,37 @@ class PrimeFinderThread implements Runnable
     @Override
     public void run()
     {
-        int numberToCheck = primeFinder.getAndIncrementCounter(), maxPrimeFactor = primeFinder.getMaxPrimeFactor();
-        while (numberToCheck <= maxPrimeFactor)
+        int number = primeFinder.getAndIncrementCounter(), maxPrimeNumber = primeFinder.getMaxPrimeNumber();
+        while (number <= maxPrimeNumber)
         {
-            checkPrimeMultiples(numberToCheck, primeFinder.getMaxPrimeNumber());
-            numberToCheck = primeFinder.getAndIncrementCounter();
+            if (isPrime(number))
+            {
+                primeFinder.addPrimeNumber(number);
+                primeFinder.incrementPrimeCount();
+                primeFinder.incrementPrimeSum(number);
+            }
+
+            number = primeFinder.getAndIncrementCounter();
         }
     }
 
-    private void checkPrimeMultiples(int number, int maxPrimeNumber)
+    private boolean isPrime(int number)
     {
-        for (int i = number * number; i <= maxPrimeNumber; i += number)
+        // Even numbers
+        if (number % 2 == 0)
         {
-            primeFinder.setIsPrime(i, true, false);
+            return number == 2;
         }
+
+        // Odd Numbers
+        int maxPrimeFactor = (int) Math.sqrt(number);
+        for (int i = 3; i <= maxPrimeFactor; i += 2)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
